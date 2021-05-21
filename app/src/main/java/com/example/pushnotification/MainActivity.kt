4aae3333363ce82a5a96d274e0bb9b8e0cd370e7
Masoud <masoud.94@yahoo.com>
2021-05-22 01:14:39 +0430

    make a butto to show notification

--- app/src/main/java/com/example/pushnotification/MainActivity.kt
+++ app/src/main/java/com/example/pushnotification/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.pushnotification
 
+import android.app.NotificationManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.pushnotification.adapter.NotificationAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val systemService = getSystemService(
+                Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        findViewById<Button>(R.id.showNotificationButton).setOnClickListener {
+            var adapter = NotificationAdapter(this@MainActivity, systemService)
+            adapter.initializeChannel()
+            adapter.initializeNotification()
+        }
     }
 }
